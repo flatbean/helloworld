d0c8d110019b8dd5081d072b6d782306deefdecf
扁豆荚 <46574179+flatbean@users.noreply.github.com>
2019-01-17 21:24:49 +0800

    老师做的修改

--- helloworld.py
+++ helloworld.py
@@ -10,5 +10,6 @@ if __name__ == '__main__':
     print('Hello, I am first python script on Github')
     print('你好，我是在github上的第一个python程序!\n')
     
-    print('你好，我里是做的第一个版本修正修改。')  
-    print('你好，我里是做的第二个版本修正修改。') 
+    print('你好，这里是做的第一个版本修正修改。')  
+    print('你好，这里是做的第二个版本修正修改。') 
+    print('你好，这里是老师做的一个版本修正修改。') 
