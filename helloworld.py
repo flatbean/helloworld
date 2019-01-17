f3edd13617c270a162613a1971e8044bd2dd3d6b
扁豆荚 <46574179+flatbean@users.noreply.github.com>
2019-01-17 21:07:08 +0800

    Create frist Github program.
    第一个Github程序版本

---
+++ helloworld.py
@@ -0,0 +1,12 @@
+#!/usr/bin/evn python
+#-*- coding: utf-8 -*-
+'''
+Created on 2019年1月17日
+
+@author: flatbean flatbean@qq.com
+'''
+
+if __name__ == '__main__':
+    print('Hello, I am first python script on Github')
+    print('你好，我是在github上的第一个python程序!\n')
+    
